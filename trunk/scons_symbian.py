865f79405ce4575da7d3e19230314f91535e6aa6
jtoivola@gmail.com <jtoivola@gmail.com@54dc1b5a-2251-0410-b086-aff3ae5240e4>
2009-11-07 17:46:02 +0000

    Merged r147 but using join for all.

--- trunk/scons_symbian.py
+++ trunk/scons_symbian.py
@@ -959,8 +959,9 @@ class SymbianProgramHandler(object):
 
         if output_lib:
             libname = self.target + ".dso"
-            self.output_libpath = ( join(ARGS.INSTALL_EPOCROOT,
-                                   r"/epoc32/release/%s/%s/%s" % ( "armv5", "lib", libname ) ) )
+            self.output_libpath = join(ARGS.INSTALL_EPOCROOT,
+                                    "epoc32","release",
+                                    "armv5", "lib", libname )
 
         build_prog = None
         if self.targettype != ARGS.TARGETTYPE_LIB:
